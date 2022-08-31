9f69e119a11e69a1ff74be7b988693dea2cc9dd5
Jeon Haejoo <40735375+ocean-joo@users.noreply.github.com>
2022-08-31 15:47:59 +0900

    add experiments file

---
+++ src/cmp_onlyLS.py
@@ -0,0 +1,16 @@
+from generator.generator import generate_tasksets
+from core.all_lockstep import get_num_core_LS
+from core.ours import get_num_core_ours
+
+def main():
+    tasks = generate_tasksets()
+
+    for task_set in tasks:
+        stateless_ts = task_set.get_tasks(sort=True, desc=True)
+        num_core_LS = get_num_core_LS(stateless_ts)
+        num_core_ours = get_num_core_ours(stateless_ts)
+
+        print(num_core_LS, num_core_ours)
+
+if __name__ == '__main__':
+    main()
