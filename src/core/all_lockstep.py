00f328970e5c29eb453ac8eefce8a1f140a0e1a0
Jeon Haejoo <40735375+ocean-joo@users.noreply.github.com>
2022-09-01 18:05:49 +0900

    update all lockstep to partitioned

--- src/core/all_lockstep.py
+++ src/core/all_lockstep.py
@@ -1,6 +1,6 @@
 import math
 from core.utils import argmin
-from core.rta import rta_all
+from core.rta import rta_all, rta_all_single
 
 def get_num_core_LS(task_set, method='deadline'):
     """
@@ -30,6 +30,8 @@ def check_schedulability(task_set, num_core, check_method):
         return check_schedulability_deadline(task_set, num_core)
     elif check_method == 'rta':
         return check_schedulability_rta(task_set, num_core)
+    elif check_method == 'rta_single':
+        return check_schedulability_rta_single(task_set, num_core)
     else:
         raise NotImplementedError
 
@@ -47,3 +49,18 @@ def check_schedulability_deadline(task_set, num_core):
 
 def check_schedulability_rta(task_set, num_core):
     return rta_all(task_set, num_core, fault=True)
+
+def check_schedulability_rta_single(task_set, num_core):
+    allocated_core = [[] for _ in range(num_core)]
+    allocated_util = [0.0 for _ in range(num_core)]
+
+    for task in task_set:
+        index = argmin(allocated_util)
+        allocated_util[index] += task[1] / task[0]
+        allocated_core[index].append(task)
+
+    for core in allocated_core:
+        if not rta_all_single(core, fault=True):
+            return False
+    else :
+        return True
