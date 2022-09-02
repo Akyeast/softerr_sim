f52a4b2124ad4ba44808e314a2831432b0bfe0e8
Jeon Haejoo <40735375+ocean-joo@users.noreply.github.com>
2022-09-02 15:26:01 +0900

    update lockstep compare experiments

--- src/cmp_onlyLS.py
+++ src/cmp_onlyLS.py
@@ -4,20 +4,16 @@ from core.all_lockstep import get_num_core_LS
 from core.ours import get_num_core_ours
 from logger.logger import Logger
 
-def main():
-    with open('cfg/task_cfg.json', 'r') as f:
-        cfg = json.load(f)
+def exp(cfg, logger):
     tasks = generate_tasksets(cfg)
-    # logger = Logger({'method': 'rta_single'})
 
     for task_set in tasks:
         stateless_ts = task_set.get_tasks(sort=True, desc=True)
-        # logger.write('{}'.format(stateless_ts))
-        print(stateless_ts)
 
         num_core_LS = get_num_core_LS(stateless_ts, method='rta_single')
         num_core_ours = get_num_core_ours(stateless_ts)
-        # logger.write('{}, {}'.format(num_core_LS, num_core_ours))
+
+        logger.write('{},{}'.format(num_core_LS, num_core_ours))
         print(num_core_LS, num_core_ours)
 
 def cmp_LSs():
@@ -31,6 +27,17 @@ def cmp_LSs():
         rta = get_num_core_LS(stateless_ts, method='rta')
         print(deadline, rta)
 
+def main():
+    with open('cfg/ls_exp_cfg.json', 'r') as f:
+        cfg = json.load(f)
+
+    for num_states in cfg['num_states_list']:
+        for criticality_prob in cfg['critical_prob_list'] :
+            new_cfg = cfg.copy()
+            new_cfg['num_states'] = num_states
+            new_cfg['critical_prob'] = criticality_prob
+            logger = Logger(new_cfg, filepath="output/ls", log_params=['num_states', 'critical_prob', 'num_tasks', 'task_max_utilization', 'period'])
+            exp(new_cfg, logger)
 
 if __name__ == '__main__':
     main()
