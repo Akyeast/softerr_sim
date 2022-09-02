de01931e13b22a55811e4f6f6d919fedff790018
Jeon Haejoo <40735375+ocean-joo@users.noreply.github.com>
2022-09-02 15:14:35 +0900

    fix logging bug

--- src/cmp_state.py
+++ src/cmp_state.py
@@ -19,7 +19,7 @@ def exp(cfg, logger):
             if num_core_statewise < num_core:
                 num_core_statewise = num_core
 
-        logger.write('{},{}\n'.format(num_core_stateless, num_core_statewise))
+        logger.write('{},{}'.format(num_core_stateless, num_core_statewise))
         print(num_core_stateless, num_core_statewise)
 
 
@@ -32,8 +32,7 @@ def main():
             new_cfg = cfg.copy()
             new_cfg['num_states'] = num_states
             new_cfg['critical_prob'] = criticality_prob
-
-            logger = Logger(cfg, log_params=['num_states', 'critical_prob', 'num_tasks', 'task_max_utilization', 'period'])
+            logger = Logger(new_cfg, filepath="output/state", log_params=['num_states', 'critical_prob', 'num_tasks', 'task_max_utilization', 'period'])
             exp(new_cfg, logger)
     
 if __name__ == '__main__':
