f9ce2a36765f9397326206aab34dede6cf1294b1
Jeon Haejoo <40735375+ocean-joo@users.noreply.github.com>
2022-09-05 12:26:21 +0900

    rename files

--- src/cmp_ls.py
+++ src/cmp_ls.py
@@ -13,7 +13,7 @@ def exp(cfg, logger):
         num_core_LS = get_num_core_LS(stateless_ts, method='rta_single')
         num_core_ours = get_num_core_ours(stateless_ts)
 
-        logger.write('{},{}'.format(num_core_LS, num_core_ours))
+        # logger.write('{},{}'.format(num_core_LS, num_core_ours))
         print(num_core_LS, num_core_ours)
 
 def cmp_LSs():
